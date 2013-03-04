c175a2324e088775ba0eb421c755e0e4d6f409a8
Alok Barsode <alok.barsode@linux.intel.com>
2013-03-04 11:53:24 +0000

    ConnMan: Add icon support

--- panels/connman/cc-network-panel.c
+++ panels/connman/cc-network-panel.c
@@ -29,6 +29,19 @@
 #include "cc-network-panel.h"
 #include "cc-network-resources.h"
 
+#define WID(b, w) (GtkWidget *) gtk_builder_get_object (b, w)
+
+enum {
+        STATUS_UNAVAILABLE,
+        STATUS_OFFLINE,
+        STATUS_NOCONNECTIONS,
+        STATUS_CONNECTING,
+        STATUS_CONNETED,
+        STATUS_ONLINE,
+        STATUS_TETHERED,
+        STATUS_LAST
+};
+
 CC_PANEL_REGISTER (CcNetworkPanel, cc_network_panel)
 
 #define NETWORK_PANEL_PRIVATE(o) \
@@ -36,6 +49,8 @@ CC_PANEL_REGISTER (CcNetworkPanel, cc_network_panel)
 
 struct _CcNetworkPanelPrivate
 {
+        gint            watch_id;
+
         GtkBuilder      *builder;
         GCancellable    *cancellable;
 };
@@ -82,16 +97,73 @@ cc_network_panel_class_init (CcNetworkPanelClass *klass)
 }
 
 static void
+network_set_status (CcNetworkPanel *panel, gint status)
+{
+        CcNetworkPanelPrivate *priv;
+        priv = NETWORK_PANEL_PRIVATE (panel);
+
+        switch (status) {
+        case STATUS_UNAVAILABLE:
+                gtk_image_set_from_icon_name (GTK_IMAGE (WID (priv->builder, "icon_status")),
+                                      "connman_offline",
+                                      GTK_ICON_SIZE_BUTTON);
+                gtk_label_set_text (GTK_LABEL (WID (priv->builder, "label_status")), _("Unavailable"));
+        default:
+                return;
+        }
+}
+
+static void
+connman_appeared_cb (GDBusConnection *connection,
+                     const gchar *name,
+                     const gchar *name_owner,
+                     gpointer user_data)
+{
+        GtkWidget *widget;
+        CcNetworkPanel *panel = user_data;
+        CcNetworkPanelPrivate *priv;
+
+        if (strcmp (name, "net.connman"))
+                return;
+
+        priv = NETWORK_PANEL_PRIVATE (panel);
+
+        widget = GTK_WIDGET (WID (priv->builder, "vbox1"));
+        gtk_widget_set_sensitive (widget, TRUE);
+}
+
+static void
+connman_disappeared_cb (GDBusConnection *connection,
+                        const gchar *name,
+                        gpointer user_data)
+{
+        GtkWidget *widget;
+        CcNetworkPanel *panel = user_data;
+        CcNetworkPanelPrivate *priv;
+
+        if (strcmp (name, "net.connman"))
+                return;
+
+        priv = NETWORK_PANEL_PRIVATE (panel);
+
+        network_set_status (panel, STATUS_UNAVAILABLE);
+
+        widget = GTK_WIDGET (WID (priv->builder, "vbox1"));
+        gtk_widget_set_sensitive (widget, FALSE);
+}
+
+static void
 cc_network_panel_init (CcNetworkPanel *panel)
 {
         GError *error = NULL;
         GtkWidget *widget;
+        CcNetworkPanelPrivate *priv;
 
-        panel->priv = NETWORK_PANEL_PRIVATE (panel);
+        priv = panel->priv = NETWORK_PANEL_PRIVATE (panel);
         g_resources_register (cc_network_get_resource ());
 
         panel->priv->builder = gtk_builder_new ();
-        gtk_builder_add_from_resource (panel->priv->builder,
+        gtk_builder_add_from_resource (priv->builder,
                                        "/org/gnome/control-center/network/network.ui",
                                        &error);
         if (error != NULL) {
@@ -100,8 +172,16 @@ cc_network_panel_init (CcNetworkPanel *panel)
                 return;
         }
 
-        panel->priv->cancellable = g_cancellable_new ();
+        priv->cancellable = g_cancellable_new ();
 
-        widget = GTK_WIDGET (gtk_builder_get_object (panel->priv->builder, "vbox1"));
+        widget = GTK_WIDGET (WID (priv->builder, "vbox1"));
         gtk_widget_reparent (widget, (GtkWidget *) panel);
+
+        priv->watch_id = g_bus_watch_name (G_BUS_TYPE_SYSTEM,
+                                           "net.connman",
+                                           G_BUS_NAME_WATCHER_FLAGS_NONE,
+                                           connman_appeared_cb,
+                                           connman_disappeared_cb,
+                                           panel,
+                                           NULL);
 }
